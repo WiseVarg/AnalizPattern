4dc2753c1f421415e3f7df0287e46695b92d38d4
WiseVarg <61451647+WiseVarg@users.noreply.github.com>
2020-02-25 22:59:59 +0500

    123

--- TreshCodeNotOpen.py
+++ TreshCodeNotOpen.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 data=pd.read_csv('Data_stock_market/SBER1D.csv',sep=';')
-print(data['<OPEN>'].values[0])
-print(data['<HIGH>'].values[0])
-print(data['<LOW>'].values[0])
-print(data['<CLOSE>'].values[0])
+#print(data['<OPEN>'].values[0])
+#print(data['<HIGH>'].values[0])
+#print(data['<LOW>'].values[0])
+#print(data['<CLOSE>'].values[0])
+def pattern1(): # Паттерны разворота бычьего тренда, 1
+    sw=0
+    sl=0
+    for i in range(1259):
+        if (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] < data['<CLOSE>'].values[i+1]) and ((data['<OPEN>'].values[i+1] - data['<LOW>'].values[i+1]) < 0.25) and (data['<OPEN>'].values[i+2] < data['<CLOSE>'].values[i+2]):
+            sw+=1
+        elif (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] < data['<CLOSE>'].values[i+1]) and ((data['<OPEN>'].values[i+1] - data['<LOW>'].values[i+1]) < 0.25) and (data['<OPEN>'].values[i+2] > data['<CLOSE>'].values[i+2]):
+            sl+=1
+    return [sw, sl]
+        
+
+print(pattern1())
