881dce2e2e34a5f104d1839edd75172780f006ae
WiseVarg <61451647+WiseVarg@users.noreply.github.com>
2020-03-06 13:32:08 +0500

    123

--- TreshCodeNotOpen.py
+++ TreshCodeNotOpen.py
@@ -9,11 +9,19 @@ def pattern1(): # Паттерны разворота бычьего тренд�
     sw=0
     sl=0
     for i in range(1259):
-        if (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] < data['<CLOSE>'].values[i+1]) and ((data['<OPEN>'].values[i+1] - data['<LOW>'].values[i+1]) < 0.25) and (data['<OPEN>'].values[i+2] < data['<CLOSE>'].values[i+2]):
+        if (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] < data['<CLOSE>'].values[i+1]) and ((data['<OPEN>'].values[i+1] - data['<LOW>'].values[i+1]) < 0.005) and (data['<OPEN>'].values[i+2] > data['<CLOSE>'].values[i+2]):
             sw+=1
-        elif (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] < data['<CLOSE>'].values[i+1]) and ((data['<OPEN>'].values[i+1] - data['<LOW>'].values[i+1]) < 0.25) and (data['<OPEN>'].values[i+2] > data['<CLOSE>'].values[i+2]):
+        elif (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] < data['<CLOSE>'].values[i+1]) and ((data['<OPEN>'].values[i+1] - data['<LOW>'].values[i+1]) < 0.005) and (data['<OPEN>'].values[i+2] < data['<CLOSE>'].values[i+2]):
             sl+=1
     return [sw, sl]
         
-
-print(pattern1())
+def pattern2(): # Паттерны разворота бычьего тренда, 4
+    sw=0
+    sl=0
+    for i in range(1259):
+        if (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] > data['<CLOSE>'].values[i+1]) and (data['<LOW>'].values[i] > data['<CLOSE>'].values[i+1]) and (data['<HIGH>'].values[i] < data['<OPEN>'].values[i+1]) and (data['<OPEN>'].values[i+2] < data['<CLOSE>'].values[i+2]):
+            sw+=1     
+        elif (data['<OPEN>'].values[i] < data['<CLOSE>'].values[i]) and (data['<OPEN>'].values[i+1] > data['<CLOSE>'].values[i+1]) and (data['<LOW>'].values[i] > data['<CLOSE>'].values[i+1]) and (data['<HIGH>'].values[i] < data['<OPEN>'].values[i+1]) and (data['<OPEN>'].values[i+2] > data['<CLOSE>'].values[i+2]):
+            sl+=1
+    return [sw, sl]
+print(pattern2())
